87329cbdc00d971312d3549b2db7bbe456cff00f
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2023-06-04 10:10:36 -0500

    dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
-      setLoading(true); // Set loading to true before making the request
+      setLoading(true);
 
       const response = await axios.post(
         'https://ttjg79x010.execute-api.us-east-2.amazonaws.com/dev',
@@ -28,62 +28,67 @@ const App: React.FC = () => {
       );
 
       const data = response.data;
-      console.log(data); // Output the response data to the console
+      console.log(data);
 
       setSongs(data.body);
 
-      setLoading(false); // Set loading back to false after receiving the response
+      setLoading(false);
     } catch (error) {
       console.error('Error:', error);
-      setLoading(false); // Set loading back to false in case of error
+      setLoading(false);
     }
   };
+
   const handleShareTwitter = () => {
     const songsText = songs.slice(0, 4).map((song, index) => `${index + 1}. ${song.name}`).join('%0A');
-  
-    
     const tweetUrl = encodeURIComponent(window.location.href);
-
     const tweetText = `Check out the top 4 songs on my Birthday Billboard!%0A${songsText}%0A%0A${tweetUrl}`;
-  
     const twitterUrl = `https://twitter.com/intent/tweet?text=${tweetText}`;
-  
     window.open(twitterUrl, '_blank');
   };
-  
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', textAlign: 'center' }}>
-      <h1 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Discover your Bithday Billboard!</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="date">Select a date: </label>
-        <input
-          type="date"
-          id="date"
-          value={selectedDate}
-          onChange={handleDateChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', minHeight: "100vh", background: '#000', color: '#fff', paddingBottom: "100px" }}>
+      <h1 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Discover your Birthday Billboard!</h1>
+      <form onSubmit={handleSubmit} style={{ display: 'flex', marginBottom: '1rem', alignItems: 'center' }}>
+  <label htmlFor="date" style={{ marginRight: '0.5rem', color: '#fff' }}>Select a date:</label>
+  <input
+  type="date"
+  id="date"
+  value={selectedDate}
+  onChange={handleDateChange}
+  max={new Date(new Date().setDate(new Date().getDate() - 1)).toISOString().split('T')[0]} // Set max attribute to the previous day's date
+  style={{ marginRight: '0.5rem', padding: "6px" }}
+/>
+  <button type="submit" style={{ backgroundColor: '#1da1f2', color: '#fff', border: 'none', padding: '0.5rem 1rem', borderRadius: '4px' }}>Submit</button>
+</form>
+
       {loading && <div style={{ marginTop: '1rem' }}>Loading...</div>}
 
       {songs?.length > 0 && !loading && (
-  <div style={{ marginTop: '2rem' }}>
-    <button style={{ marginTop: '1rem' }} onClick={handleShareTwitter}>Share on Twitter</button>
-    {songs.map((song) => (
-      <div key={song.id} style={{ marginTop: '1rem' }}>
-        <iframe
-          src={`https://open.spotify.com/embed/track/${song.id}`}
-          width="100%"
-          height="80"
-          frameBorder="0"
-          allow="encrypted-media"
-          title={song.name}
-        ></iframe>
-      </div>
-    ))}
-  </div>
-)}
+        <div>
+          <button style={{ marginTop: '1rem', backgroundColor: '#1da1f2', color: '#fff', border: 'none', padding: '0.5rem 1rem', borderRadius: '4px', cursor: 'pointer' }} onClick={handleShareTwitter}>
+            Share on Twitter
+          </button>
+          {songs.map((song, i) => (
+            <div key={song.id} style={{ marginTop: '1rem', display: "flex", alignItems: "center" }}>
 
+                
+              <p>#{i+1} </p>
+              
+              
+              <iframe
+                src={`https://open.spotify.com/embed/track/${song.id}`}
+                width="100%"
+                height="80"
+                frameBorder="0"
+                allow="encrypted-media"
+                title={song.name}
+              ></iframe>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
